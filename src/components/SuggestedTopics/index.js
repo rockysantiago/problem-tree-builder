5ae4b9d967637f7b7497b9b7d0d40dce1ed42242
Rocky Santiago <raphael.santiago@icloud.com>
2019-08-15 10:38:56 +0800

    add onClick handler for suggested topics

--- src/components/SuggestedTopics/index.js
+++ src/components/SuggestedTopics/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
+import { navigate } from '@reach/router';
 
 const SuggestedTopics = props => (
   <>
     {props.suggestedTopics.map(suggestedTopic => (
-      <Button basic size="mini" content={suggestedTopic} key={suggestedTopic} />
+      <Button
+        basic
+        size="mini"
+        content={suggestedTopic}
+        key={suggestedTopic}
+        onClick={() => navigate(`search/${suggestedTopic}`)}
+      />
     ))}
   </>
 );

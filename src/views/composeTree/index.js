a07d8f0e62f3b72723983025bbc219303dfc2642
Rocky Santiago <raphael.santiago@icloud.com>
2019-08-29 16:56:09 +0800

    update generate tree

--- src/views/composeTree/index.js
+++ src/views/composeTree/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { navigate } from '@reach/router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Grid, Header } from 'semantic-ui-react';
+import { Button, Grid, Header } from 'semantic-ui-react';
 
 import SearchBar from 'components/SearchBar';
 import SearchInput from 'components/SearchInput';
@@ -101,6 +102,9 @@ class ComposeTree extends Component {
       <Grid padded style={{ height: '100vh', overflow: 'hidden' }}>
         <Grid.Column width={11} style={{ height: '100%' }}>
           {/* Effects section */}
+          <Button onClick={() => navigate('/generate-tree')}>
+            Generate Tree
+          </Button>
           <Tree data={topic} onDeleteSibling={this.handleSelectResult} />
           {/* {topic.effects.length === 0 && topic.problem.text && (
             <button onClick={() => this.initAddWithType('effect')}>
--- src/views/generateTree/index.js
+++ src/views/generateTree/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { saveAs } from 'file-saver';
 import html2canvas from 'html2canvas';
+import { Button } from 'semantic-ui-react';
+
+import Tree from 'components/Tree';
 
 class GenerateTree extends Component {
   printDocument = async () => {
@@ -10,8 +14,19 @@ class GenerateTree extends Component {
   };
 
   render() {
-    return <div id="capture">GenerateTree</div>;
+    const { topic } = this.props;
+
+    return (
+      <>
+        <div id="capture">
+          <Tree data={topic} />
+        </div>
+        <Button onClick={this.printDocument}>Export File</Button>
+      </>
+    );
   }
 }
 
-export default GenerateTree;
+const mapStateToProps = state => state;
+
+export default connect(mapStateToProps)(GenerateTree);

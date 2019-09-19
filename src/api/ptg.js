1a89820cfe8c3c1fb798755d4092c5a68183bf5c
Rocky Santiago <raphael.santiago@icloud.com>
2019-09-19 18:14:51 +0800

    update api

--- src/api/ptg.js
+++ src/api/ptg.js
@@ -73,3 +73,45 @@ export const sendUserScore = async (stars, id) => {
     );
   }
 };
+
+export const addUserInput = async (type, text, link, problem) => {
+  const options = getOptions({ action: 'add', type, text, link, problem });
+
+  try {
+    let response = await fetch(`${keys.baseUrl}/get_user_action`, options);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+
+    response = await response.json();
+
+    return response.data;
+  } catch (error) {
+    console.error(
+      'There has been a problem with your fetch operation: ',
+      error.message
+    );
+  }
+};
+
+export const updateProblem = async (text, id) => {
+  const options = getOptions({ action: 'edit', text, id });
+
+  try {
+    let response = await fetch(`${keys.baseUrl}/get_user_action`, options);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+
+    response = await response.json();
+
+    return response.data;
+  } catch (error) {
+    console.error(
+      'There has been a problem with your fetch operation: ',
+      error.message
+    );
+  }
+};
